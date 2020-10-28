984d5c68f487cadabb7b29d00310e948cecd043b
wzq <710147356@qq.com>
2020-10-28 13:40:40 +0800

    feat: 奇偶链表

---
+++ 234.回文链表.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=javascript
+ *
+ * [234] 回文链表
+ *
+ * https://leetcode-cn.com/problems/palindrome-linked-list/description/
+ *
+ * algorithms
+ * Easy (43.56%)
+ * Likes:    745
+ * Dislikes: 0
+ * Total Accepted:    164.9K
+ * Total Submissions: 367.3K
+ * Testcase Example:  '[1,2]'
+ *
+ * 请判断一个链表是否为回文链表。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1->2
+ * 输出: false
+ * 
+ * 示例 2:
+ * 
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 
+ * 
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+
+};
+// @lc code=end
+
