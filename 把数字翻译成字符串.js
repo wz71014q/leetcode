2ae614ee5a25eca49e6cc8aee22060f88761b4bf
wzq <710147356@qq.com>
2020-11-25 19:05:07 +0800

    动态规划—递归优化

--- 把数字翻译成字符串.js
+++ 把数字翻译成字符串.js
@@ -17,8 +17,9 @@
 /**
  * @param {number} num
  * @return {number}
- * @description 该题与爬楼梯类似，使用动态规划法，状态转移方程是dp(i) = dp(i - 1) + dp(i - 2)，但要排除不合法的数字组合
- * 时间复杂度O(n)，空间复杂度O(n)
+ * @description 普通递归，从上往下，依赖于下一个节点的值
+ * 时间复杂度O(Math.pow(2, n))，空间复杂度O(n)
+ * 存在重复计算的问题，子树计算优化可以参考二叉树
  */
 var translateNum = function (num) {
   var bigger = num % 100 > 25 || num % 100 < 10; // 排除大于25的或者06这种
@@ -31,4 +32,49 @@ var translateNum = function (num) {
   return bigger ? translateNum(parseInt(num / 10)) : translateNum(parseInt(num / 10)) + translateNum(parseInt(num / 100));
 };
 
-// TODO 优化算法
+// 优化算法
+// 一、存储已知值的递归
+// 将已知的值存起来，减少运算次数，时间复杂度O(n)，空间复杂度O(n)
+var translateNum = function (num) {
+  var memory = {};
+  var dfs = function (num) {
+    if (memory[num]) {
+      return memory[num];
+    }
+    var invalid = num % 100 > 25 || num % 100 < 10; // 排除大于25的或者06这种
+    var numsArray = `${num}`.split('');
+    var arrayLength = numsArray.length;
+    if (arrayLength <= 2) {
+      return num <= 25 ? arrayLength : 1;
+    }
+    memory[num] = (invalid ? dfs(parseInt(num / 10)) : dfs(parseInt(num / 10)) + dfs(parseInt(num / 100)));
+    return memory[num];
+  }
+  return dfs(num);
+};
+
+// 二、动态规划，从下往上(画出树的结构，从最下面已知的case开始)，由已知结果求上面的问题，时间复杂度O(n)，空间复杂度O(1)
+// 状态转移方程是dp(i) = dp(i - 1) + dp(i - 2); (10 <= num <= 25)
+//              dp(i) = dp(i - 1); (num > 25)
+var translateNum = function (num) {
+  var str = `${num}`;
+  var numLength = str.length;
+  if (numLength < 2) {
+    return numLength;
+  }
+  var result = 0;
+  var f0 = 1;
+  var f1 = 1;
+  var curNumber = 0;
+  for (var i = 2; i <= numLength; i++) {
+    curNumber = Number(str[i - 2] + str[i - 1]);
+    if (curNumber > 9 && curNumber < 26) {
+      result = f1 + f0;
+    } else {
+      result = f1;
+    }
+    f0 = f1;
+    f1 = result;
+  }
+  return result;
+};
