d61159c654e9419e7064a55054d7699ad7835d2a
wzq <710147356@qq.com>
2021-04-16 13:49:55 +0800

    feat: 150栈

---
+++ 150.逆波兰表达式求值.js
@@ -0,0 +1,139 @@
+/*
+ * @lc app=leetcode.cn id=150 lang=javascript
+ *
+ * [150] 逆波兰表达式求值
+ * [150] Evaluate Reverse Polish Notation
+ * https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/description/
+ *
+ * algorithms
+ * Medium (51.19%)
+ * Likes:    333
+ * Dislikes: 0
+ * Total Accepted:    107.9K
+ * Total Submissions: 200.7K
+ * Testcase Example:  '["2","1","+","3","*"]'
+ *
+ * 根据 逆波兰表示法，求表达式的值。
+ * 
+ * 有效的算符包括 +、-、*、/ 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+ * 
+ * 
+ * 
+ * 说明：
+ * 
+ * 
+ * 整数除法只保留整数部分。
+ * 给定逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：tokens = ["2","1","+","3","*"]
+ * 输出：9
+ * 解释：该算式转化为常见的中缀算术表达式为：((2 + 1) * 3) = 9
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：tokens = ["4","13","5","/","+"]
+ * 输出：6
+ * 解释：该算式转化为常见的中缀算术表达式为：(4 + (13 / 5)) = 6
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+ * 输出：22
+ * 解释：
+ * 该算式转化为常见的中缀算术表达式为：
+ * ⁠ ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+ * = ((10 * (6 / (12 * -11))) + 17) + 5
+ * = ((10 * (6 / -132)) + 17) + 5
+ * = ((10 * 0) + 17) + 5
+ * = (0 + 17) + 5
+ * = 17 + 5
+ * = 22
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * tokens[i] 要么是一个算符（"+"、"-"、"*" 或 "/"），要么是一个在范围 [-200, 200] 内的整数
+ * 
+ * 
+ * 
+ * 
+ * 逆波兰表达式：
+ * 
+ * 逆波兰表达式是一种后缀表达式，所谓后缀就是指算符写在后面。
+ * 
+ * 
+ * 平常使用的算式则是一种中缀表达式，如 ( 1 + 2 ) * ( 3 + 4 ) 。
+ * 该算式的逆波兰表达式写法为 ( ( 1 2 + ) ( 3 4 + ) * ) 。
+ * 
+ * 
+ * 逆波兰表达式主要有以下两个优点：
+ * 
+ * 
+ * 去掉括号后表达式无歧义，上式即便写成 1 2 + 3 4 + * 也可以依据次序计算出正确结果。
+ * 适合用栈操作运算：遇到数字则入栈；遇到算符则取出栈顶两个数字进行计算，并将结果压入栈中。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+ var evalRPN = function (tokens) {
+  if (!tokens || !tokens.length) {
+    return;
+  }
+  const operators = {
+    '+': 1,
+    '-': 2,
+    '*': 3,
+    '/': 4
+  };
+  function cacl(o, m, n) {
+    let caclResult;
+    switch (operators[o]) {
+      case 1:
+        caclResult = m + n;
+        break;
+      case 2:
+        caclResult = m - n;
+        break;
+      case 3:
+        caclResult = parseInt(m * n);
+        break;
+      case 4:
+        caclResult = parseInt(m / n);
+        break;
+    }
+    return caclResult;
+  }
+  const operandStack = [];
+  let current;
+  while (tokens.length) {
+    let current = tokens.shift();
+    if (operators[current]) {
+      let operand1 = parseInt(operandStack.pop());
+      let operand2 = parseInt(operandStack.pop());
+      operandStack.push(cacl(current, operand2, operand1));
+    } else {
+      operandStack.push(current);
+    }
+  }
+  return operandStack[0];
+};
+// @lc code=end
+
