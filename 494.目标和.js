2bd1aed27ff3674e30e72a097d6e033a991c8f4f
wzq <710147356@qq.com>
2021-04-27 09:58:00 +0800

    feat: 494 目标和

---
+++ 494.目标和.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=494 lang=javascript
+ *
+ * [494] 目标和
+ *
+ * https://leetcode-cn.com/problems/target-sum/description/
+ *
+ * algorithms
+ * Medium (44.88%)
+ * Likes:    650
+ * Dislikes: 0
+ * Total Accepted:    80K
+ * Total Submissions: 175.9K
+ * Testcase Example:  '[1,1,1,1,1]\n3'
+ *
+ * 给定一个非负整数数组，a1, a2, ..., an, 和一个目标数，S。现在你有两个符号 + 和 -。对于数组中的任意一个整数，你都可以从 + 或
+ * -中选择一个符号添加在前面。
+ * 
+ * 返回可以使最终数组和为目标数 S 的所有添加符号的方法数。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：nums: [1, 1, 1, 1, 1], S: 3
+ * 输出：5
+ * 解释：
+ * 
+ * -1+1+1+1+1 = 3
+ * +1-1+1+1+1 = 3
+ * +1+1-1+1+1 = 3
+ * +1+1+1-1+1 = 3
+ * +1+1+1+1-1 = 3
+ * 
+ * 一共有5种方法让最终目标和为3。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 数组非空，且长度不会超过 20 。
+ * 初始的数组的和不会超过 1000 。
+ * 保证返回的最终结果能被 32 位整数存下。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ * @description DFS递归，这题迭代法的边界条件没找到，暂时用递归
+ */
+ var findTargetSumWays = function (nums, target) {
+  let result = 0;
+  const dfs = function (_nums, _target, index, sum) {
+    if (index >= _nums.length) {
+      if (sum == _target) {
+        result++;
+      }
+      return;
+    }
+    dfs(_nums, _target, index + 1, sum + _nums[index]);
+    dfs(_nums, _target, index + 1, sum - _nums[index]);
+  };
+  dfs(nums, target, 0, 0);
+  return result;
+};
+// @lc code=end
+
